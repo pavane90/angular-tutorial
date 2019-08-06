25ec1c223287ab74a32cefd51221406b031528e3
pavane90 <pavane90@gmail.com>
2019-08-06 23:46:56 +0900

    Services - 3 files modified, 1 added

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
 import { products } from '../products';
+import { CartService } from '../cart.service';
 
 @Component({
   selector: 'app-product-details',
@@ -10,10 +11,16 @@ import { products } from '../products';
 })
 export class ProductDetailsComponent implements OnInit {
   product;
+  addToCart(product) { //addToCart를 정의함
+    window.alert('Your product has been added to the cart!');
+    this.cartService.addToCart(product);
+  }
 
   constructor(
     private route: ActivatedRoute,
     //ActivatedRoute은 Angular 라우터가 컴포넌트를 로드할 때 사용한 라우팅 규칙을 의미합니다. 이 객체에는 현재 사용된 라우팅 규칙, 라우팅 변수 등의 데이터가 들어있습니다.
+    private cartService: CartService
+    //등록한 cart.service를 의존성으로 주입함 (injection)
   ) { }
 
   ngOnInit() {
